661b337876192460fc2bd21235fd017c63c3af17
Secret Company <secretcompany70@gmail.com>
2025-05-03 00:26:41 +0800

    Update lt-hash.ts

--- src/Utils/lt-hash.ts
+++ src/Utils/lt-hash.ts
@@ -15,47 +15,54 @@ class d {
 	constructor(e: string) {
 		this.salt = e
 	}
+
 	add(e, t) {
-		var r = this
-		for(const item of t) {
-			e = r._addSingle(e, item)
+		for (const item of t) {
+			e = this._addSingle(e, item)
 		}
-
 		return e
 	}
+
 	subtract(e, t) {
-		var r = this
-		for(const item of t) {
-			e = r._subtractSingle(e, item)
+		for (const item of t) {
+			e = this._subtractSingle(e, item)
 		}
-
 		return e
 	}
-	subtractThenAdd(e, t, r) {
-		var n = this
-		return n.add(n.subtract(e, r), t)
-	}
-	async _addSingle(e, t) {
-		var r = this
-		const n = new Uint8Array(await hkdf(Buffer.from(t), o, { info: r.salt })).buffer
-		return r.performPointwiseWithOverflow(await e, n, ((e, t) => e + t))
-	}
-	async _subtractSingle(e, t) {
-		var r = this
 
-		const n = new Uint8Array(await hkdf(Buffer.from(t), o, { info: r.salt })).buffer
-		return r.performPointwiseWithOverflow(e, n, ((e, t) => e - t))
+	subtractThenAdd(e, t, r) {
+		return this.add(this.subtract(e, r), t)
 	}
+
+	async _addSingle(e, t) {
+		const n = new Uint8Array(await hkdf(Buffer.from(t), o, { info: this.salt })).buffer
+		return this.performPointwiseWithOverflow(await e, n, ((e, t) => e + t))
+	}
+
+	async _subtractSingle(e, t) {
+		const n = new Uint8Array(await hkdf(Buffer.from(t), o, { info: this.salt })).buffer
+		return this.performPointwiseWithOverflow(e, n, ((e, t) => e - t))
+	}
+
 	performPointwiseWithOverflow(e, t, r) {
+		if (!(e instanceof ArrayBuffer)) {
+			throw new Error('Invalid first argument: expected ArrayBuffer, got ' + typeof e)
+		}
+		if (!(t instanceof ArrayBuffer)) {
+			throw new Error('Invalid second argument: expected ArrayBuffer, got ' + typeof t)
+		}
+
 		const n = new DataView(e)
-		  , i = new DataView(t)
-		  , a = new ArrayBuffer(n.byteLength)
-		  , s = new DataView(a)
-		for(let e = 0; e < n.byteLength; e += 2) {
+		const i = new DataView(t)
+		const a = new ArrayBuffer(n.byteLength)
+		const s = new DataView(a)
+
+		for (let e = 0; e < n.byteLength; e += 2) {
 			s.setUint16(e, r(n.getUint16(e, !0), i.getUint16(e, !0)), !0)
 		}
 
 		return a
 	}
 }
+
 export const LT_HASH_ANTI_TAMPERING = new d('WhatsApp Patch Integrity')
